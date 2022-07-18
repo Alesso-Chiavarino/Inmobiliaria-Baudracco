e2a22d3a34755fa9a4c4460b53bcb043432ffec9
alesso-chiavarino <alessochiavarino@gmail.com>
2022-07-18 04:17:09 -0300

    muchoscambios

--- js/alquiler.js
+++ js/alquiler.js
@@ -22,3 +22,61 @@ window.onload = function() {
     $('#onload').fadeOut();
     $('body').removeClass('hidden');
 }
+
+
+// carousel
+const swiper = new Swiper('.swiper', {
+	loop: true,
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
+	},
+	pagination: {
+		el: '.swiper-pagination',
+		type: 'bullets',
+		clickable: true,
+	},
+	keyboard: true,
+	mousewheel: true,
+
+	// effect: 'creative',
+	// creativeEffect: {
+	// 	prev: {
+	// 		// will set `translateZ(-400px)` on previous slides
+	// 		translate: [0, 0, -400],
+	// 	},
+	// 	next: {
+	// 		// will set `translateX(100%)` on next slides
+	// 		translate: ['100%', 0, 0],
+	// 	},
+	// },
+	// effect: 'coverflow',
+});
+
+// modal
+
+let modal = document.getElementById('miModal');
+let contModal = document.getElementById('contModal');
+let abrir = document.getElementById('casa1');
+let cerrar = document.getElementById('cruz');
+let bodyy = document.getElementById('bodyy');
+
+abrir.addEventListener('click', function(){
+    modal.style.display = 'block';
+    bodyy.style.overflow = 'hidden';
+    
+});
+
+cerrar.addEventListener('click', function(){
+        modal.style.display = 'none';
+		bodyy.style.overflow = 'visible';
+    });
+    
+    window.addEventListener('click', function(e){
+        console.log(e.target);
+        if(e.target == contModal){
+            modal.style.display = 'none';
+            bodyy.style.overflow = 'visible';
+    }
+});
+
